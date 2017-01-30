2cd557f7e2d81b10fabfc87a08bd420fb848c614
Drake Scott <dascott0204@gmail.com>
2017-01-29 20:24:23 -0600

    Finished both exercises.

--- app.js
+++ app.js
@@ -2,5 +2,23 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0
+        },
+        computed: {
+            output: function() {
+                return this.value != 37 ? 'Done' : 'Not There Yet';
+            }
+        },
+        watch: {
+            value: function(value) {
+                var vm = this;
+                setTimeout(function() {
+                    vm.value = 0;
+                }, 5000);
+            }
+        },
+        methods: {
+            result: function() {
+                return this.value >= 37 ? 'Done' : 'Not There Yet';
+            }
         }
     });
